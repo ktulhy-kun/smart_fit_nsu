c185e2bc010ace5b663a66a469f4f10c53573f1e
Korovin Viktor <sarvash.inc@gmail.com>
2016-12-24 05:48:11 +0700

    Научился доставать новости

--- fit_nsu_grabber.py
+++ fit_nsu_grabber.py
@@ -1,6 +1,8 @@
+from lxml import etree
 from lxml import html
 from datetime import datetime
 
+from lxml.html import Element
 from lxml.html import HtmlElement
 import urllib3
 
@@ -55,17 +57,42 @@ class Category:
         for tr in items_raw:
             self.items.append(NewsItem(self.domain, tr))
 
+        for item in self.items:
+            item()
+
     def __str__(self):
         return "Category {}({})".format(self.name, self.href)
 
 
 class NewsItem:
-    def __init__(self, domain: str, tr):
+    def __init__(self, domain: str, tr: Element):
         self.domain = domain
         a = tr.find(".//a")
         self.head = a.text.rstrip().lstrip()
         self.href = a.attrib['href']
         self.date = tr.find_class('list-date')[0].text.rstrip().lstrip()
+        self.content = None  # type: Element
+
+    def __call__(self):
+        print("Get page {}".format(self.href))
+        page = html.parse(self.domain + self.href)
+        print("Ok")
+        content = page.getroot().find_class('item-page')[0]  # type: Element
+        h2 = content.find(".//h2")
+        dls = content.find_class("article-info")
+        content.remove(h2)
+        for dl in dls:
+            content.remove(dl)
+        for tag in content.iter():  # type: Element
+            if 'style' in tag.attrib:
+                del tag.attrib['style']
+
+        self.content = content
+
+    def __str__(self):
+        if self.content:
+            return etree.tostring(self.content, pretty_print=True)
+        return "==Content Missing=="
 
 
 news_p = NewsParser(DOMAIN, NEWS)
